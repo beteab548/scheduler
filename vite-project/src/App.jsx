27c0d91c5f8bf600e70f468cc031b79ab0a8d297
beteab548 <beteabbaynessagne548@gmail.com>
2024-09-07 12:46:13 -0700

    styled the header

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import clockImage from "./assets/clock-image.avif";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-       </>
-  )
+    <div className="flex-col content-center  h-screen w-screen">
+      <div className="w-80 h-80 grid ml-custom-px mb-80 ">
+        <img className="p-6 mt-4 ml-20 h-28  " src={clockImage} alt="" />
+        <p className="font-semibold mb-44 ml-10 pl-10">
+          welcome to <span className="font-bold text-cyan-600">scheduler</span>
+        </p>
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
