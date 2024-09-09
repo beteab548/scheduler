e1cc23209fdc027af386f7aadaf633c55b47604a
beteab548 <beteabbaynessagne548@gmail.com>
2024-09-08 22:37:48 -0700

    added more components

--- vite-project/src/components/month.jsx
+++ vite-project/src/components/month.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 export default function Month({ monthNumber }) {
   return (
     <>
-      <div className="bg-red-900 h-52 w-50 ">first</div>
+      <div className="bg-red-900 h-52 w-50 rounded-lg">first</div>
+      <div className="bg-red-900 h-52 w-50 rounded-lg">first</div>
+      <div className="bg-red-900 h-52 w-50 rounded-lg">first</div>
     </>
   );
 }
