8fe63c9fc9de6fbff7d79d4d79e64d9d4212672c
beteab548 <beteabbaynessagne548@gmail.com>
2024-09-09 22:16:55 -0700

    little modification to header

--- vite-project/src/components/month.jsx
+++ vite-project/src/components/month.jsx
@@ -3,7 +3,6 @@ export default function Month({ monthNumber }) {
   return (
     <>
       <div className="bg-red-900 h-52 w-50 rounded-lg">first</div>
-      <div className="bg-red-900 h-52 w-50 rounded-lg">second</div>
     </>
   );
 }
