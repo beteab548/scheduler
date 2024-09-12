d55a918e7529db6e5301bc2ad32529b0be55f42c
beteab548 <beteabbaynessagne548@gmail.com>
2024-09-11 21:55:25 -0700

    month component updated

--- vite-project/src/components/month.jsx
+++ vite-project/src/components/month.jsx
@@ -3,7 +3,7 @@ export default function Month({ monthNumber }) {
   return (
     <>
       <div className="bg-red-900 h-52 w-50 rounded-lg">first</div>
-      <div className="bg-red-900 h-52 w-50 rounded-lg">first</div>
+      <div className="bg-red-900 h-52 w-50 rounded-lg">second</div>
     </>
   );
 }
